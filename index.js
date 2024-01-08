0b0727593edb6856397377d76e903e72541c41e9
nicole-cab <nc482@exeter.ac.uk>
2024-01-08 02:41:44 +0000

    city api used, weather api in progress, next -> check for invalid inputs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // server/index.js
 
 const express = require("express");
-const request = require("request");
 const app = express();
 
 app.use(express.static("mycities-project/dist"));
@@ -19,46 +18,146 @@ app.get("/api", (req, res) => {
 // const con = db.connectDB();
 
 // --------------------------------------------------
+const multer = require("multer");
+const upload = multer();
+const https = require("https");
+const fs = require("fs");
 
-const API_KEY = "2xbPkHlrLtyWfRQ7tw2dyA==zzglcyZcsk3PHeQ3";
+// cityInfo json
+const cityInfoPath = "cityInfo.json";
 
-let multer = require("multer");
-let upload = multer();
+// read the keys from the api_keys.json file
+const apiKeysPath = "api_keys.json";
+const apiNinjas_API_KEY = readFromFile(apiKeysPath, "api_ninjas");
+const openWeather_API_KEY = readFromFile(apiKeysPath, "open_weather");
 
-function getCity(cityName) {
-  let lat, lon;
-  return request.get(
-    {
-      url: "https://api.api-ninjas.com/v1/city?name=" + cityName,
-      headers: {
-        "X-Api-Key": API_KEY,
-      },
-    },
-    function (error, response, body) {
-      if (error) return console.error("Request failed:", error);
-      else if (response.statusCode != 200)
-        return console.error(
-          "Error:",
-          response.statusCode,
-          body.toString("utf8")
-        );
-      else {
-        // let lat = response.body;
-        let body = JSON.parse(response.body)[0];
-        lat = body["latitude"];
-        lon = body["longitude"];
-      }
+function readFromFile(path, key) {
+  try {
+    // read the json
+    let json = fs.readFileSync(path);
+    json = JSON.parse(json);
+    console.log("> Data successfully READ from file");
+    // return value of key
+    return json[key];
+  } catch (error) {
+    console.log("An error has occurred ", error);
+  }
+}
+
+console.log("api-ninjas: ", apiNinjas_API_KEY);
+console.log("open_weather: ", openWeather_API_KEY);
+
+function appendToFile(path, key, value) {
+  // create file if it doesn't already exist and add the city info
+  try {
+    let json;
+    // read the json file if it exists or else create one
+    if (fs.existsSync(path)) {
+      json = fs.readFileSync(path);
+      json = JSON.parse(json);
+    } else {
+      json = {};
     }
-  );
+
+    // append new key-value pair
+    json[key] = value;
+
+    // update json with new key-value pair
+    fs.writeFileSync(path, JSON.stringify(json), "utf8");
+    console.log("> File UPDATED successfully");
+  } catch (error) {
+    console.log("An error has occurred ", error);
+  }
 }
 
 app.post("/city", upload.fields([]), (req, res) => {
   const cityName = req.body.cityName;
-  // console.log("getCity: " + JSON.stringify(getCity(cityName)));
-  // const lat = getCity(cityName)["lat"];
-  // const lon = getCity(cityName)["lon"];
-  // res.json({ success: "true", lat: lat, lon: lon });
+
+  console.log("openweather: ", openWeather_API_KEY);
+  // get country from city api then the weather from the weather api
+  getCity(cityName);
 });
+
+function getCity(cityName) {
+  // use the Api Ninjas City API
+  const options = {
+    hostname: "api.api-ninjas.com",
+    port: 443,
+    path: "/v1/city?name=" + cityName,
+    headers: {
+      "X-Api-Key": apiNinjas_API_KEY,
+    },
+  };
+  https
+    .get(options, function (res) {
+      let data = "";
+
+      // a chunk of data has been received
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      // the whole response has been received
+      res.on("end", () => {
+        // parse data
+        let body = JSON.parse(data)[0];
+        console.log("city api: ", body);
+
+        console.log("SUCCESSFULLY USED CITY API");
+
+        // save country to json file
+        appendToFile(cityInfoPath, "country_code", body.country);
+
+        // get weather of city from weather api
+        getWeather(cityName);
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+}
+
+function getWeather(cityName) {
+  // get country code from json file
+  let countryCode = readFromFile(cityInfoPath, "country_code");
+
+  // use the openWeather API
+  const options = {
+    hostname: "api.openweathermap.org",
+    port: 443,
+    path:
+      "/data/2.5/weather?q=" +
+      cityName +
+      "," +
+      countryCode +
+      "&appid=" +
+      openWeather_API_KEY,
+  };
+
+  https
+    .get(options, function (res) {
+      let data = "";
+
+      // a chunk of data has been received
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      // the whole response has been received
+      res.on("end", () => {
+        // parse data
+        let body = JSON.parse(data);
+        console.log(body);
+
+        console.log("SUCCESSFULLY USED WEATHER API");
+
+        // save/append weather data to json file
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
